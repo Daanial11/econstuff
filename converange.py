9744f58eaab03830bfd665a0fb1f8b350d88982b
Daanial11 <daanial74@gmail.com>
2021-12-08 00:59:21 +0000

    static buyer random

--- converange.py
+++ converange.py
@@ -63,11 +63,11 @@ for n in range(6):
     duration = 275 * 100 * (2+(2*n))
     print(n)
     range1 = (50, 150)
-    supply_schedule = [{'from': start_time, 'to': duration, 'ranges': [range1], 'stepmode': 'jittered'}
+    supply_schedule = [{'from': start_time, 'to': duration, 'ranges': [range1], 'stepmode': 'random'}
                     ]
 
     range2 = (50, 150)
-    demand_schedule = [{'from': start_time, 'to': duration, 'ranges': [range2], 'stepmode': 'jittered'}
+    demand_schedule = [{'from': start_time, 'to': duration, 'ranges': [range2], 'stepmode': 'random'}
                     ]
 
     order_sched = {'sup': supply_schedule, 'dem': demand_schedule,
